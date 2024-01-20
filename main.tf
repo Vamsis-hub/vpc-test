f9706a3254c856593c1e20cb5ba497ae176f8438
vamsis-hub <vamsireddychaganti.19@gmail.com>
2024-01-20 18:05:40 +1000

    terraform-vpc

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "roboshop" {
-    source ="../terraform-aws-vpc"
+    # source ="../terraform-aws-vpc"
+    source ="git::https://github.com/Vamsis-hub/terraform-aws-vpc.git?ref=main"
     project_name = var.project_name
     environment_name = var.environment_name
     common_tags =var.common_tags
@@ -11,3 +12,4 @@ module "roboshop" {
     is_peering_required = var.is_peering_required
     
 }
+
